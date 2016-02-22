f75c422e06ccd45aaa95887fa7033d1b0720e013
trestin bradley <trestin.bradley@SkyKick.com>
2016-02-22 09:52:41 -0500

    updated styling - center and space

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,37 @@
 import React from 'react';
 
 class App extends React.Component {
-  render() {
-      return (
-        <p>{this.props.phrase}</p>
-      );
+    render() {
+        var rowsPad = {
+            marginTop: 10
+        };
+
+        return (
+            <div>
+                <div className="jumbotron">
+                    <div className="container">
+                        <h1>{this.props.phrase}</h1>
+                        <p>shorten your urls!</p>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="panel panel-default">
+                        <div className="panel-heading">
+                            <div className="row">
+                                <div className="col-md-12">
+                                    <input type="text" className="form-control" placeholder="Enter an url..." />
+                                </div>
+                            </div>
+                            <div className="row" style={rowsPad}>
+                                <div className="col-md-12">
+                                    <button type="button" className="btn btn-success">Shorten!</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
