ce0f74a05812d3efe0498c8a67f6a7ebc3acbfda
trestin bradley <trestin.bradley@SkyKick.com>
2016-02-22 10:35:58 -0500

    package update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import React from 'react';
 import App from './components/app';
 
 React.render(
-  <App phrase="Hello Earl"/>,
+  <App foo="abc" bar="123" phrase="Hello Earl" />,
   document.getElementById('rootDiv')
 );
